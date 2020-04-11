328beac7c4dcc56ba301cfcc943bb24ae6926df5
朱元昊 <zhuyuanhao@zhuyuanhaodeMacBook-Pro.local>
2020-04-12 02:03:14 +0800

    commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+        function check () {
+          var name = document.getElementById('name').value
+          var name1 = document.getElementById('name1').value
+          var name2 = document.getElementById('name2').value
+          if (name == null || name == '') {
+            alert('You forget to fill them all!')
+            return false
+          } else if (name1 == null || name1 == '') {
+            alert('You forget to fill them all!')
+            return false
+          } else if (name2 == null || name2 == '') {
+            alert('You forget to fill them all!')
+            return false
+          }
+          alert('Submit successful!')
+          return true
+        }
+     
